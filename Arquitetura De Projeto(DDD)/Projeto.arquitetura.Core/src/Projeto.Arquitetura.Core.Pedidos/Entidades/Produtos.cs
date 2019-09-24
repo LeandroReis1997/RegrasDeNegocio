2ed7f583f723040fb0089c663b17e6e2fcee894d
Leandro Reis <leandro.visualc@gmail.com>
2019-09-23 23:28:50 -0300

    Validações em produtos

--- Arquitetura De Projeto(DDD)/Projeto.arquitetura.Core/src/Projeto.Arquitetura.Core.Pedidos/Entidades/Produtos.cs
+++ Arquitetura De Projeto(DDD)/Projeto.arquitetura.Core/src/Projeto.Arquitetura.Core.Pedidos/Entidades/Produtos.cs
@@ -1,4 +1,6 @@
 ﻿using Prj.Arq.Core.Dom.Shared.Entidades;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Projeto.Arquitetura.Core.Domain.Pedidos.Entidades
 {
@@ -12,7 +14,31 @@ namespace Projeto.Arquitetura.Core.Domain.Pedidos.Entidades
 
         public override bool EstaConsistente()
         {
-            throw new System.NotImplementedException();
+            ApelidoDeveSerPreenchido();
+            ApelidoDeveTerUmtamanhoLimite();
+            ValorDeveraserSuperiorAZero();
+            UnidadeDeveSerValida();
+            return !ListaErros.Any();
+        }
+        private void ApelidoDeveSerPreenchido()
+        {
+            if (string.IsNullOrEmpty(Apelido))
+                ListaErros.Add("O campo apelido deve ser preenchido!");
+        }
+        private void ApelidoDeveTerUmtamanhoLimite()
+        {
+            if (Apelido.Length > 20)
+                ListaErros.Add("O apelido deve ter 20 caracters!");
+        }
+        private void ValorDeveraserSuperiorAZero()
+        {
+            if (Valor <= 0)
+                ListaErros.Add("O campo valor deve ser maior que zero!");
+        }
+        private void UnidadeDeveSerValida()
+        {
+            var listUnidade = new List<string> { "KL", "GR", "MT", "CM", "QT" };
+            if (!listUnidade.Contains(Unidade)) ListaErros.Add("Unidade deve ser KL, GR, MT, CM ou QT!");
         }
     }
 }
